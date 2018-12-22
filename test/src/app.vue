bcfe1ff3dc3717475d5f99dbff2c404a117e498e
WittBulter <nanazuimeng123@gmail.com>
2018-12-22 12:37:20 +0800

    feat: improve examples

--- test/src/app.vue
+++ test/src/app.vue
@@ -1,19 +1,43 @@
 <template>
 <div id="app">
-  <HelloWorld/>
+  <div class="doc">
+    <button @click="to('https://github.com/HaiFengFE/v-decorators')">查看文档</button>
+  </div>
+  <demo/>
 </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import demo from './components/demo'
 
 export default {
   name: 'app',
 
   components: {
-    HelloWorld,
+    demo,
+  },
+
+  methods: {
+    to(url) {
+      window.open(url)
+    },
   },
 }
 </script>
 
-<style lang="css" src="./style.css"/>
+<style src="./style.css"/>
+<style>
+.doc {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 40px;
+  background: #f7f6f6;
+  display: flex;
+  padding: 0 20px;
+  justify-content: flex-start;
+  align-items: center;
+}
+</style>
+
